839bd465f5df20fb28c3110895666144c00b4115
Dilshani Abeywickrama <dilshanig@xdoto.io>
2020-11-30 08:41:50 +0530

    image added

--- src/components/Boxes/Boxes.jsx
+++ src/components/Boxes/Boxes.jsx
@@ -6,7 +6,7 @@ const Boxes =(props) => {
     return (
         <div xs={12} md={3} className={styles.topic}>
 
-            <img className={styles.logo } src="https://th.bing.com/th/id/OIP.YqXSiXJqX6SZSLi8CNEHGwHaHb?pid=Api&rs=1" width="3%" alt="logo" />
+            <img className={styles.logo } src="/images/image.png" width="3%" alt="logo" />
             <h1>COVID-19: Live Situational Analysis Dashboard</h1>
         </div>
     );
